4dbe229fb1c460a65bdc145bc6fb9dc3d93d10c9
PARTH_DS <parthvashishtha498@gmail.com>
2026-07-23 09:56:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -155,4 +155,8 @@ This project predicts the **overall evaluation of a car** based on:
 """
     )
 
-demo.launch()
+if __name__ == "__main__":
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=int(os.environ.get("PORT", 7860))
+    )
